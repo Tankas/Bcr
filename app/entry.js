7ea81ed8e68d867a2d716bb4bd0ff40642acf0b0
Tankas <984086816@qq.com>
2019-08-20 19:22:15 +0800

    MAINT: 使用proxy代替defineProperty

--- app/entry.js
+++ app/entry.js
@@ -19,27 +19,40 @@ let ct = new Bcr({
     return {
       name: 'tanka',
       age: 18,
+      ff: {
+        name: 'ffff'
+      },
+      arr: [1,2,3]
     }
   },
   render () {
     return <div>
-      <Button></Button>
       <h1>title</h1>
       <p>
+        <p>{this.arr[0]}</p>
         <p>name:{this.name}</p>
         <p>
           <span onClick={this.test}>age::{this.age}</span>
         </p>
       </p>
       <div>
-        sadfads
+        {this.arr[9]}
       </div>
     </div>
   },
   methods: {
     test() {
-      this.age ++
-      this.name = 'change'
+      this.arr[0] ++
     }
   }
 })
+
+function createTypeFunc (type) {
+  return function (obj) {
+    return Object.prototype.toString.call(obj) === '[object ' + type + ']'
+  }
+}
+
+let isObject = createTypeFunc('Object')
+let isArray = createTypeFunc('Array')
+
--- app/init.js
+++ app/init.js
@@ -1,24 +1,75 @@
+const watch = (object, onChange) => {
+  const handler = {
+    get (target, property, receiver) {
+      try {
+        return new Proxy(target[property], handler)
+      } catch (err) {
+        return Reflect.get(target, property, receiver)
+      }
+    },
+    defineProperty (target, property, descriptor) {
+      onChange(target[property], descriptor) // 赋值时触发回调函数
+      return Reflect.defineProperty(target, property, descriptor)
+    },
+    deleteProperty (target, property) {
+      onChange() // 删除属性时触发回调函数
+      return Reflect.deleteProperty(target, property)
+    }
+  }
+  return new Proxy(object, handler)
+}
+
+function dataChange () {
+  setTimeout(() => {
+    this._update()
+  }, 0)
+}
+
 export function initData (vm, opts) {
   vm.options = opts
   vm.el = opts.el
   let data = opts.data()
-  // 深度遍历所有的data
-  for (let prop in data) {
+  let proxyData = watch(data, dataChange.bind(vm))
+  for (let prop in proxyData) {
     // vm[prop] = data[prop] // proxy
     Object.defineProperty(vm, prop , {
       get () {
-        return data[prop]
+        return proxyData[prop]
       },
       set (newValue) {
-        console.log('change', newValue)
-        data[prop] = newValue
+        proxyData[prop] = newValue
         // 批处理
         // vdom diff
-        vm._update()
+        //vm._update()
       }
     })
   }
+  console.log(vm)
+
+  // 深度遍历所有的data
+  // for (let prop in data) {
+  //   // vm[prop] = data[prop] // proxy
+  //   Object.defineProperty(vm, prop , {
+  //     get () {
+  //       return data[prop]
+  //     },
+  //     set (newValue) {
+  //       console.log('change', newValue)
+  //       data[prop] = newValue
+  //       // 批处理
+  //       // vdom diff
+  //       vm._update()
+  //     }
+  //   })
+  // }
 }
+
+// obj 中的所有属性都代理到vm上面 
+
+
+
+
+
 export function initProps (vm, opts) {
   vm.props = {}
 }
